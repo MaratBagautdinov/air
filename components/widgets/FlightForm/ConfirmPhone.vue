86167a8816861a0ad95bffecee4e97533edc8670
Marat <marat.bagautdinov522@mail.ru>
2024-09-03 14:25:59 +0300

    confirm email and sms

--- components/widgets/FlightForm/ConfirmPhone.vue
+++ components/widgets/FlightForm/ConfirmPhone.vue
@@ -18,7 +18,7 @@
       <InputRow
         label="Код подтверждения"
         placeholder="Код подтверждения"
-        type="number"
+        type="string"
         v-model="userData.code"
         :required="true"
       />
@@ -29,6 +29,9 @@
           type="submit"
         />
       </div>
+      <p style="color: orangered" class="text-20 pt-3" v-if="formState.error = true">
+        {{ formState.msg ?? 'Ошибка подтверждения' }}
+      </p>
     </form>
     <div
       v-else
@@ -57,13 +60,22 @@
     pending: false,
     sended: false,
     error: false,
+    msg: ''
   });
+  const route = useRoute();
   const handleConfirmPhone = async () => {
     formState.value.sended = false;
     formState.value.pending = true;
-
+    formState.value.msg = '';
     try {
-      const res = await $fetch(useApiUrl() + "auth/confirm_sms", {
+      const res = await $fetch<{
+        error: string
+        status: number
+      } | {
+        access_token: string
+        need_data: boolean
+        refresh_token: string
+      }>(useApiUrl() + "auth/confirm_sms", {
         method: "POST",
         body: {
           phone: userData.value.phone,
@@ -71,12 +83,24 @@
         },
       });
 
-      if (res) {
+      if ('access_token' in res) {
         formState.value.sended = true;
+        const confirmRes = await $fetch(useApiUrl() + "pre-orders", {
+          method: "POST",
+          body: {
+            access_token: res.access_token,
+            card_id: route.params.slug,
+          },
+        });
+        console.log(confirmRes)
+      } else {
+        formState.value.error = true;
+        formState.value.msg = 'Не верный код';
       }
     } catch (e) {
       console.error(e);
       formState.value.error = true;
+      formState.value.msg = 'Не верный код';
     } finally {
       formState.value.pending = false;
     }
